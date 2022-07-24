0318a4466a37be922b96ca4f72487672efcf17c2
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-07-24 13:46:11 +0530

    Homepage blueprint

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head><title>Rock Paper Scissors</title></head>
-    
+    <body>
+        <h1>Welcome to the Rock Paper Scissors Game!!!</h1>
+        <p>
+            <a href="">Please Log In</a>
+        </p>
+        <p>Attempt to go to 
+        <a href="">game.php</a> without looging in - it should fail with an error message.</p>
+    </body>
 </html>
