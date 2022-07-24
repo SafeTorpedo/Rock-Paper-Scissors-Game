c216b06fb3baef493df427b6ac63eaafb9946e4c
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-07-24 21:56:09 +0530

    Final links updation

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <body>
         <h1>Welcome to the Rock Paper Scissors Game!!!</h1>
         <p>
-            <a href="">Please Log In</a>
+            <a href="login.php">Please Log In</a>
         </p>
         <p>Attempt to go to 
-        <a href="">game.php</a> without looging in - it should fail with an error message.</p>
+        <a href="game.php">game.php</a> without looging in - it should fail with an error message.</p>
     </body>
 </html>
